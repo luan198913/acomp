84b213d61387b804701d3b3374bd3f4b3a02cd10
Luan Ramos <102614994+luan198913@users.noreply.github.com>
2022-04-19 17:59:13 +0000

    bubble

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import numpy as np
-from time import process_time_ns as ns
+import bubble
+bubble.main()
 
-def max(v):
-  m = v[0] #(1)
-  for i in range(1, len(v)): #(2)
-    if v[i] > m: #(3)
-      m = v[i] #(4)
-  return m #(5)
 
-v = np.random.randint(1, 100, 100)
-s=sorted(v)
-r=sorted(v, reverse= True)
 
-n1 = ns()
-m =max(s)
-n2 =ns()
 
-print(max(v))
-print(n2-n1)
